7f9b9dd6719beaf4dbacabdd022bf9a96acba830
pie-suhee <tngmltngml4@naver.com>
2023-02-13 15:20:12 +0900

    Update main.js

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -23,6 +23,7 @@ window.addEventListener("DOMContentLoaded",()=>{
     // 2. 클릭이벤트 함수 설정하기
     // for of사용! -> 배열/컬렉션 일때 사용!
     for(let x of mlist){ // x 각 li요소
+
         x.onclick = ()=>{
 
             // 1. li에 클래스 초기화!
@@ -38,7 +39,56 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     } ///////// for of /////////////
 
+    /******************************************** 
+        [ 극장가는 길 구글맵보기 기능 ]
+        - 극장가는길 박스 클릭시 구글맵 등장
+        - 구글맵박스의 닫기버튼 클릭시 구글맵 퇴장
+        -> 방법: CSS셋팅된 클래스"on"넣기/빼기
+    ********************************************/
 
+    const anibx = qs(".anibx");
+
+    const gmap = qs(".gmap");
+
+    const cbtn = qs(".cbtn");
+
+    anibx.onclick = ()=>{
+        gmap.classList.add("on");
+        anibx.classList.add("on");
+    };
+
+    cbtn.onclick = ()=>{
+        gmap.classList.remove("on");
+        anibx.classList.remove("on");
+    };
+    
+    const sns = qsa(".sns a");
+    
+    sns.forEach((ele)=>{
+        ele.onclick = ()=>{
+            let btxt = ele.innerText;
+            console.log(btxt);
+
+            let url = "";
+
+            switch(btxt){
+                case"페이스북 바로가기" :
+                    url="https://www.facebook.com/CJCGV";
+                    break;
+                case"트위터 바로가기" :
+                    url="https://twitter.com/CGV_ID";
+                    break;
+                case"인스타그램 바로가기" :
+                    url="https://www.instagram.com/cgv_korea/";
+                    break;
+
+            }
+
+            // 새창 -> window.open()
+            // 페이지 이동 -> location.href = url값
+            window.open().location.href = url;
+        };
+    }); // forEach
 
 
 }); ///////////// 로드구역 ////////////////
